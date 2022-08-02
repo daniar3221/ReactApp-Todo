51b4a549cff8a6bcc258d2713f12acc03e3dea46
Daniar <daniar322228@mail.ru>
2022-08-02 08:57:44 +0300

    commit without eslint

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -7,130 +7,128 @@ import TodoList from '../todo-list';
 import TodoFooter from '../todo-footer';
 
 export default class App extends Component {
-  constructor() {
-    super();
-    this.maxId = 100;
-    this.state = {
-      todoData: [
-        {
-          id: 1,
-          value: 'Drink coffee',
-          editing: false,
-          date: new Date('2022-06-01'),
-          done: false,
-        },
-        {
-          id: 2,
-          value: 'Make a lunch',
-          editing: false,
-          date: new Date('2022-07-26'),
-          done: false,
-        },
-        {
-          id: 3,
-          value: 'Build Todo Project',
-          editing: false,
-          date: new Date(),
-          done: false,
-        },
-      ],
-      actualButton: 'all',
-    };
-    this.changeDoneItem = (id) => {
-      this.setState(({ todoData }) => {
-        const changingItem = todoData.find((item) => item.id === id);
-        const changingItemClone = { ...changingItem };
-        changingItemClone.done = !changingItemClone.done;
-        const idx = todoData.findIndex((item) => item.id === id);
-        const newArray = [
-          ...todoData.slice(0, idx),
-          changingItemClone,
-          ...todoData.slice(idx + 1),
-        ];
+  
+  maxId = 100;
+  state = {
+    todoData: [
+      {
+        id: 1,
+        value: 'Drink coffee',
+        editing: false,
+        date: new Date('2022-06-01'),
+        done: false,
+      },
+      {
+        id: 2,
+        value: 'Make a lunch',
+        editing: false,
+        date: new Date('2022-07-26'),
+        done: false,
+      },
+      {
+        id: 3,
+        value: 'Build Todo Project',
+        editing: false,
+        date: new Date(),
+        done: false,
+      },
+    ],
+    actualButton: 'all',
+  };
+  deleteItem = (id) => {
+    this.setState(({ todoData }) => {
+      const idx = todoData.findIndex((item) => item.id === id);
+      const newArray = [
+        ...todoData.slice(0, idx),
+        ...todoData.slice(idx + 1),
+      ];
+      return {
+        todoData: newArray,
+      };
+    });
+  };
+  changeItemText = (text, id) => {
+    this.setState(({ todoData }) => {
+      const editingItem = todoData.find((item) => item.id === id);
+      const editingItemClone = { ...editingItem };
+      editingItemClone.value = text;
+      editingItemClone.editing = false;
+      const idx = todoData.findIndex((item) => item.id === id);
+      const newArray = [
+        ...todoData.slice(0, idx),
+        editingItemClone,
+        ...todoData.slice(idx + 1),
+      ];
+      return {
+        todoData: newArray,
+      };
+    });
+  };
+  changeEditStatusItem = (id) => {
+    this.setState(({ todoData }) => {
+      const editingItem = todoData.find((item) => item.id === id);
+      const editingItemClone = { ...editingItem };
+      editingItemClone.editing = !editingItemClone.editing;
+      const idx = todoData.findIndex((item) => item.id === id);
+      const newArray = [
+        ...todoData.slice(0, idx),
+        editingItemClone,
+        ...todoData.slice(idx + 1),
+      ];
+      return {
+        todoData: newArray,
+      };
+    });
+  };
+  deleteAllCompleted = () => {
+    this.setState(({ todoData }) => {
+      const completedTasks = todoData.filter((item) => !item.done);
+      return {
+        todoData: completedTasks,
+      };
+    });
+  };
+  changeActualButton = (actBtn) => {
+    this.setState({
+      actualButton: actBtn,
+    });
+  };
+  addItem = (value) => {
+    this.setState(({ todoData }) => {
+      const newItem = {
+        id: this.maxId++,
+        value,
+        editing: false,
+        date: new Date(),
+        done: false,
+      };
+      const newArray = [
+        ...todoData,
+        newItem,
+      ];
+      return {
+        todoData: newArray,
+      };
+    });
+  };
+  changeDoneItem = (id) => {
+    this.setState(({ todoData }) => {
+      const changingItem = todoData.find((item) => item.id === id);
+      const changingItemClone = { ...changingItem };
+      changingItemClone.done = !changingItemClone.done;
+      const idx = todoData.findIndex((item) => item.id === id);
+      const newArray = [
+        ...todoData.slice(0, idx),
+        changingItemClone,
+        ...todoData.slice(idx + 1),
+      ];
 
-        return {
-          todoData: newArray,
-        };
-      });
-    };
-    this.deleteItem = (id) => {
-      this.setState(({ todoData }) => {
-        const idx = todoData.findIndex((item) => item.id === id);
-        const newArray = [
-          ...todoData.slice(0, idx),
-          ...todoData.slice(idx + 1),
-        ];
-        return {
-          todoData: newArray,
-        };
-      });
-    };
-    this.addItem = (value) => {
-      this.setState(({ todoData }) => {
-        const newItem = {
-          id: this.maxId++,
-          value,
-          editing: false,
-          date: new Date(),
-          done: false,
-        };
-        const newArray = [
-          ...todoData,
-          newItem,
-        ];
-        return {
-          todoData: newArray,
-        };
-      });
-    };
-    this.changeActualButton = (actBtn) => {
-      this.setState({
-        actualButton: actBtn,
-      });
-    };
-    this.deleteAllCompleted = () => {
-      this.setState(({ todoData }) => {
-        const completedTasks = todoData.filter((item) => !item.done);
-        return {
-          todoData: completedTasks,
-        };
-      });
-    };
-    this.changeEditStatusItem = (id) => {
-      this.setState(({ todoData }) => {
-        const editingItem = todoData.find((item) => item.id === id);
-        const editingItemClone = { ...editingItem };
-        editingItemClone.editing = !editingItemClone.editing;
-        const idx = todoData.findIndex((item) => item.id === id);
-        const newArray = [
-          ...todoData.slice(0, idx),
-          editingItemClone,
-          ...todoData.slice(idx + 1),
-        ];
-        return {
-          todoData: newArray,
-        };
-      });
-    };
-    this.changeItemText = (text, id) => {
-      this.setState(({ todoData }) => {
-        const editingItem = todoData.find((item) => item.id === id);
-        const editingItemClone = { ...editingItem };
-        editingItemClone.value = text;
-        editingItemClone.editing = false;
-        const idx = todoData.findIndex((item) => item.id === id);
-        const newArray = [
-          ...todoData.slice(0, idx),
-          editingItemClone,
-          ...todoData.slice(idx + 1),
-        ];
-        return {
-          todoData: newArray,
-        };
-      });
-    };
-  }
-
+      return {
+        todoData: newArray,
+      };
+    });
+  };
+  
   render() {
     
     const { todoData, actualButton } = { ...this.state };
--- src/components/editing-input/editing-input.jsx
+++ src/components/editing-input/editing-input.jsx
@@ -5,34 +5,33 @@ import PropTypes from 'prop-types';
 
 export default class EditingInput extends Component {
 
-  constructor() {
-    super();
-    // const { itemText, itemId, editItemText } = this.props;
-    this.state = {
+  state = {
+    value: this.props.itemText,
+  };
+  
+  onLabelChange = (e) => {
+    this.setState({
+      value: e.target.value,
+    });
+  };
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.props.editItemText(this.state.value, this.props.itemId);
+    this.setState({
       value: '',
-    };
-    this.onLabelChange = (e) => {
-      this.setState({
-        value: e.target.value,
-      });
-    };
-    this.onSubmit = (e) => {
-      e.preventDefault();
-      this.props.editItemText(this.state.value, this.props.itemId);
-      this.setState({
-        value: '',
-      });
-    };
-  }
+    });
+  };
 
   render() {
+    // const { itemText, itemId, editItemText } = this.props
+    const { value } = this.state
     return (
       <form onSubmit={this.onSubmit}>
         <input
           onChange={this.onLabelChange}
           className="editing-input"
           placeholder="Type something to change..."
-          value={this.state.value}
+          value={value}
         />
       </form>
     );
